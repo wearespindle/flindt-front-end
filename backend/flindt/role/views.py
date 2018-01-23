c7e653c8950b7d674510d1e7bbd791c858451d57
Joël Kuijten <joel@pm5544.eu>
2018-01-23 16:12:12 +0100

    mirrored the standard repo and threw away all back end code to mkae this is front end only repo
    This will make developping and deploying Flindt easier

--- backend/flindt/role/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-
-from .models import Role
-from .serializers import RoleSerializer
-
-
-class RoleViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Role.objects.all()
-    serializer_class = RoleSerializer
